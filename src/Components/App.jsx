47fce2c135cbd51ada440a1fc05a622e858448ce
Greepsu <arnaud.mgn2@gmail.com>
2021-05-17 11:51:57 +0200

    Change p font-size and "Lift up" event

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,33 +1,35 @@
-import React, { useRef } from "react"
+import React, { useRef } from "react";
 
 //import style
-import '../styles/App.css';
+import "../styles/App.css";
 
 //import Components
-import Contact from './Contact';
-import Navbar from './Navbar';
-import Presentation from './Presentation';
-import Project from './Project';
+import Contact from "./Contact";
+import Navbar from "./Navbar";
+import Presentation from "./Presentation";
+import Project from "./Project";
 
 function App() {
-
   //Define ref
-  const presentationRef = useRef()
-  const projectRef = useRef()
-  const contactRef = useRef()
+  const presentationRef = useRef();
+  const projectRef = useRef();
+  const contactRef = useRef();
+
+    //Scroll to the ref section
+    const executeScroll = (ref) => ref.current.scrollIntoView({ behavior: "smooth", block: "start" });
+
 
   return (
     <div className="App">
       <div className="app-container">
-      <Navbar props={{ presentationRef, projectRef, contactRef }} />
-      <Presentation ref={presentationRef} />
-      <Project ref={projectRef} />
-      <Contact ref={contactRef} />
-      <div className="separator"></div>
-      <div className="lift-up">
-        <a href="#Home">Lift up</a>
-      </div>
-
+        <Navbar props={{ presentationRef, projectRef, contactRef }} />
+        <Presentation ref={presentationRef} />
+        <Project ref={projectRef} />
+        <Contact ref={contactRef} />
+        <div className="separator"></div>
+        <div className="lift-up">
+          <a href="#Home" onClick={() => executeScroll(presentationRef)}>Lift up</a>
+        </div>
       </div>
     </div>
   );
