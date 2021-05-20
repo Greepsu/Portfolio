40b23b047416a1fed0b01c22121a7814db6fc196
Greepsu <arnaud.mgn2@gmail.com>
2021-05-20 11:25:07 +0200

    import anmation GSAP

--- src/Components/Presentation.jsx
+++ src/Components/Presentation.jsx
@@ -1,14 +1,24 @@
-import React, { forwardRef } from 'react'
+import React, { useEffect, forwardRef } from 'react'
 
 //import styles
 import "../styles/Presentation.css"
 
+//import GSAP  
+import { gsap } from "gsap"
+
 function Presentation({ props }, ref) {
+    useEffect(() => {
+        let tl = gsap.timeline({defaults: {ease: "SlowMo.easeout"}})
+
+        // tl.to(".presentation", {y: '100%', duration:1.5, delay:0.5});
+        tl.fromTo("h1", {opacity: 0, y:'100%'}, {opacity: 1, y:'0%', delay:0.5});
+    }, [])
+
     return (
         <div className="presentation" ref={ref} >
             <div className="presentation-title">
                 <p>My approach is minimalist with clarity and balance in mind</p>
-                <h1>Hello! I'm Arnaud, a web developper based in France</h1>
+                <h1><span id="create" >Hello! I'm Arnaud, a web developper based in France</span></h1>
             </div>
             <div className="presentation-section">
                 <h2>What I do ?</h2>
